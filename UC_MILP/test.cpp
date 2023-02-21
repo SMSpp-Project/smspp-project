dfedbce4e98a3bbd24b8d1f349c9d2eabdbf1654
Donato Meoli <donato.meoli.95@gmail.com>
2023-02-21 13:46:53 +0100

    fixed names

--- UC_MILP/test.cpp
+++ UC_MILP/test.cpp
@@ -202,8 +202,8 @@ TEST_P ( UCMILPTest, Solve ) {
 INSTANTIATE_TEST_SUITE_P( UCMILPTests , UCMILPTest , ::testing::Values(
  // file, ub
  // Cooperative case without and with explicit ECNetworkBlock(s)
- TestParameter{ "data/EC_Test.nc4" , 3507796.2761932584 } ,
- TestParameter{ "data/EC_Test_NB.nc4" , 3507796.2761932584 } ,
+ TestParameter{ "data/EC_CO_Test.nc4" , 3507796.2761932584 } ,
+ TestParameter{ "data/EC_CO_Test_NB.nc4" , 3507796.2761932584 } ,
  // Non-Cooperative case due to No Assets installed, without
  // and with explicit NetworkBlock(s) respectively
  TestParameter{ "data/EC_NA_Test.nc4" , 4177364.156161939 } ,
