a7785ea741afa594f8eff17fc347ebd78d307290
dmeoli <donato.meoli.95@gmail.com>
2024-06-27 23:26:02 +0200

    updated INSTALL.ps1

--- INSTALL.ps1
+++ INSTALL.ps1
@@ -1,5 +1,4 @@
 # RUN THIS SCRIPT FROM A "DEVELOPER POWERSHELL FOR VS" AS ADMINISTRATOR
-# VISUAL STUDIO (WITH THE ENGLISH LANGUAGE PACK) WITH THE "DESKTOP DEVELOPMENT WITH C++" IS NEEDED
 
 # Default value indicating if CPLEX should be installed
 param(
@@ -19,6 +18,16 @@ if ($OS -eq "Win32NT")
     Set-Location "C:\"
 
     Write-Host "Starting the installation process on Windows..."
+
+    # Install Visual Studio (English language pack) with the "Desktop Development with C++"
+    if (-not (Test-Path "C:\Program Files\Microsoft Visual Studio"))
+    {
+        Write-Host "Installing Microsoft Visual Studio compiler..."
+        $VISUAL_STUDIO_INSTALLER = "VisualStudioSetup.exe"
+        Invoke-WebRequest -Uri "https://c2rsetup.officeapps.live.com/c2r/downloadVS.aspx?sku=community&channel=Release&version=VS2022&source=VSLandingPage&cid=2030:108d217f1e244b9aa0326ce9a131978a" -OutFile $VISUAL_STUDIO_INSTALLER
+        Start-Process -FilePath $VISUAL_STUDIO_INSTALLER -Wait
+        Remove-Item $VISUAL_STUDIO_INSTALLER
+    }
 
     # Install basic requirements using Chocolatey
     Write-Host "Installing basic requirements..."
