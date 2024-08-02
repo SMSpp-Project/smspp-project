c329047c946b6bfd02e98ceb7c4f35e8f7a67402
dmeoli <donato.meoli.95@gmail.com>
2024-08-02 05:25:34 +0200

    updated INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -92,12 +92,11 @@ install_on_ubuntu() {
           curl -o "$CPLEX_INSTALLER" "$CPLEX_URL&export=download&authuser=0&confirm=t&uuid=$uuid"
           chmod u+x "$CPLEX_INSTALLER"
           cat <<EOL > silentinstall.properties
-ACCEPT_LICENSE=true
+ACCEPT_LICENSE=TRUE
 USER_INSTALL_DIR=$CPLEX_ROOT
 EOL
           ./"$CPLEX_INSTALLER" -i silent -f silentinstall.properties
           rm "$CPLEX_INSTALLER" silentinstall.properties
-          #mv ./ibm/ILOG/CPLEX_Studio2211 "$CPLEX_ROOT"
           export CPLEX_HOME="$CPLEX_ROOT/cplex"
           export PATH="${PATH}:${CPLEX_HOME}/bin/x86-64_linux"
           export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${CPLEX_HOME}/lib/x86-64_linux"
