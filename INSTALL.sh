5d4a0bf2354509cbaff60e61bcd507121a04d723
dmeoli <donato.meoli.95@gmail.com>
2024-08-02 18:46:43 +0200

    updated INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -97,8 +97,7 @@ LICENSE_ACCEPTED=TRUE
 USER_INSTALL_DIR=$CPLEX_ROOT
 EOL
           # run the CPLEX installer in a subshell
-          setsid ./"$CPLEX_INSTALLER" -f ./installer.properties &
-          wait $!
+          ( ./"$CPLEX_INSTALLER" -f ./installer.properties & wait $! )
           INSTALLER_EXIT_CODE=$?
           if [ $INSTALLER_EXIT_CODE -eq 0 ]; then
             rm "$CPLEX_INSTALLER" installer.properties
