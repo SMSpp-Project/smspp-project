92ac47a349e2167983e80c071d9b5324db030bd3
dmeoli <donato.meoli.95@gmail.com>
2024-08-03 13:28:43 +0200

    updated INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -560,7 +560,8 @@ cd cmake-build-debug
 cmake -DCMAKE_INSTALL_PREFIX="$CMAKE_PREFIX" -DCMAKE_BUILD_TYPE=Debug -Wno-dev ..
 # run ccmake in a subshell
 sh -c "ccmake .." & # select submodules, then Configure and Generate the build files
-wait # wait for ccmake to finish
+# pause script until user presses enter
+read -p "Press Enter to continue after finishing ccmake configuration..."
 cmake --build . --config Debug
 cmake --install . --config Debug
 cd ..
