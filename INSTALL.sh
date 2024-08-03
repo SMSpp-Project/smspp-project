977dd9304f5109195d8443be11157e352b4bc99c
dmeoli <donato.meoli.95@gmail.com>
2024-08-03 13:22:42 +0200

    updated INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -558,10 +558,8 @@ fi
 mkdir -p cmake-build-debug
 cd cmake-build-debug
 cmake -DCMAKE_INSTALL_PREFIX="$CMAKE_PREFIX" -DCMAKE_BUILD_TYPE=Debug -Wno-dev ..
-# run ccmake in a xterm subshell
-# (calling gnome-terminal as a subshell does not work with sudo)
-xterm -e ccmake .. & # select submodules, then Configure and Generate the build files
-wait # wait for ccmake to finish
+# run ccmake in a subshell
+sh -c "ccmake .." # select submodules, then Configure and Generate the build files
 cmake --build . --config Debug
 cmake --install . --config Debug
 cd ..
@@ -570,10 +568,8 @@ cd ..
 mkdir -p cmake-build-release
 cd cmake-build-release
 cmake -DCMAKE_INSTALL_PREFIX="$CMAKE_PREFIX" -DCMAKE_BUILD_TYPE=Release -Wno-dev ..
-# run ccmake in a xterm subshell
-# (calling gnome-terminal as a subshell does not work with sudo)
-xterm -e ccmake .. & # select submodules, then Configure and Generate the build files
-wait # wait for ccmake to finish
+# run ccmake in a subshell
+sh -c "ccmake .." # select submodules, then Configure and Generate the build files
 cmake --build . --config Release
 cmake --install . --config Release
 cd ..
