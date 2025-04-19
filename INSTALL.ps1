198bc37d163dae6e55dbba48a17f1f6a509f6f18
dmeoli <donato.meoli.95@gmail.com>
2025-04-20 00:25:42 +0200

    updated INSTALL.ps1

--- INSTALL.ps1
+++ INSTALL.ps1
@@ -456,7 +456,7 @@ if (-not $withoutSMSpp)
             "-DCMAKE_INSTALL_PREFIX=$SMSPP_ROOT/Debug" `
             '-DCMAKE_BUILD_TYPE=Debug' `
             "-DCMAKE_TOOLCHAIN_FILE=$env:VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake" `
-            '-Wno-dev' # `
+            '-Wno-dev' #`
             #'-DBUILD_SHARED_LIBS=ON'
     # run cmake-gui
     Start-Process -FilePath "cmake-gui" -ArgumentList "build/Debug" -Wait # select submodules, then Configure and Generate the build files
@@ -471,7 +471,7 @@ if (-not $withoutSMSpp)
             "-DCMAKE_INSTALL_PREFIX=$SMSPP_ROOT/Release" `
             '-DCMAKE_BUILD_TYPE=Release' `
             "-DCMAKE_TOOLCHAIN_FILE=$env:VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake" `
-            '-Wno-dev' # `
+            '-Wno-dev' #`
             #'-DBUILD_SHARED_LIBS=ON'
     # run cmake-gui
     Start-Process -FilePath "cmake-gui" -ArgumentList "build/Release" -Wait # select submodules, then Configure and Generate the build files
