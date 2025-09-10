3c050417b5b26e3442a7255af5ea6410463eb363
dmeoli <donato.meoli.95@gmail.com>
2025-09-10 18:29:03 +0200

    updated INSTALL.ps1

--- INSTALL.ps1
+++ INSTALL.ps1
@@ -214,6 +214,7 @@ if ($OS -eq "Win32NT")
     if (-not $withoutCoinOr) {
         Write-Host "Configuring COIN-OR Osi..."
         Set-Location $env:VCPKG_ROOT
+        .\vcpkg install coinutils --triplet x64-windows
 
         if (-not $withoutGurobi) {
             Write-Host "Modifying COIN-OR Osi portfile.cmake for Gurobi interface..."
