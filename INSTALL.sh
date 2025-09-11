f57cdee83651bae0606b2eaa3db1ea3fb87b9c00
dmeoli <donato.meoli.95@gmail.com>
2025-09-11 23:42:57 +0200

    updated INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -744,12 +744,7 @@ case "$OS" in
   ;;
 "Darwin")
   INSTALL_ROOT="${install_root:-/Library}"
-  # Check if the user has sudo access
-  if [ "$(id -u)" -eq 0 ] || sudo -n true 2>/dev/null; then
-    SMSPP_ROOT="${INSTALL_ROOT}/smspp-project"
-  else
-    SMSPP_ROOT="${HOME}/smspp-project"
-  fi
+  SMSPP_ROOT="${INSTALL_ROOT}/smspp-project"
   install_on_macos
   ;;
 *)
