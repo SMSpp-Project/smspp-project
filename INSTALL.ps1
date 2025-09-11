bcc67beb2e38138803d21ab8c699fb4df0a72132
dmeoli <donato.meoli.95@gmail.com>
2025-09-11 04:32:19 +0200

    updated INSTALL.ps1

--- INSTALL.ps1
+++ INSTALL.ps1
@@ -298,11 +298,13 @@ if ($OS -eq "Win32NT")
         # Re-install COIN-OR Osi only if we actually modified the portfile
         if ($RebuildCoinOrOsi) {
             Write-Host "Re-installing COIN-OR Osi..."
+            $env:VCPKG_FEATURE_FLAGS = "registries" # no manifests here
             & "$env:VCPKG_ROOT\vcpkg.exe" remove coin-or-osi --triplet x64-windows --recurse
             & "$env:VCPKG_ROOT\vcpkg.exe" install coin-or-osi coin-or-clp --triplet x64-windows `
                                                                           --x-install-root "$InstallRootOverride" `
                                                                           --no-binarycaching `
                                                                           --clean-after-build
+            $env:VCPKG_FEATURE_FLAGS = "manifests,registries" # restore previous value
         }
         Write-Host " done."
     }
