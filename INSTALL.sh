c5a152a1e8bc2157a5e217ea5c1a700992b8d0aa
dmeoli <donato.meoli.95@gmail.com>
2025-10-03 09:41:30 +0200

    updated submodules

--- INSTALL.sh
+++ INSTALL.sh
@@ -66,7 +66,7 @@ install_on_linux() {
 
     # Install Boost libraries
     echo "Installing Boost libraries..."
-    apt-get install -y -q libboost-dev libboost-system-dev libboost-timer-dev libboost-mpi-dev libboost-random-dev
+    apt-get install -y -q libboost-dev libboost-timer-dev libboost-mpi-dev
 
     # Install OpenMP
     echo "Installing OpenMP..."
